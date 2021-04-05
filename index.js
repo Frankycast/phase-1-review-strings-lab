2a600ad138237a17db16a22959f70f51f4c6013c
Francisco Castillo <Francisco21castillo@aol.com>
2021-04-05 10:54:50 -0400

    commit tree d584733345f0c8d0e232dac6ad3472aac334cdff

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Francisco Castillo'; 
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser+'!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()+ currentUser+'!';
+const shortGreeting = 'Welcome, '+currentUser[0]+'!';
+
+ 
